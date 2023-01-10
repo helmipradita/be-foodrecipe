8e784f727c8ad7c01c91839a6cbecc01dc225a2c
Helmi Pradita <helmipraditaa@gmail.com>
2023-01-11 02:59:00 +0700

    add cookie-parser and update cors

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const cors = require(`cors`);
+const cookieParser = require('cookie-parser');
 const morgan = require(`morgan`);
 const bodyParser = require(`body-parser`);
 require(`dotenv`).config();
@@ -11,7 +12,14 @@ const port = process.env.PORT;
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.use(cors());
+const corsOptions = {
+  origin: 'http://localhost:3000',
+  credentials: true, //access-control-allow-credentials:true
+  optionSuccessStatus: 200,
+};
+app.use(cors(corsOptions));
+
+app.use(cookieParser());
 app.use(morgan('dev'));
 
 app.use('/', mainRouter);
